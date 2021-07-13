550fd62428414c01080660a59cf56bf9ff986f82
Memo Jimenez <memojl08@gmail.com>
2021-07-13 16:38:20 -0500

    update api

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,11 +1,12 @@
 /* VARIABLES CONSTANTES*/
 var loc = window.location;
+const host = loc.host;
 const dominio = loc.origin + '/';
 const path_url1 = loc.pathname;
 var path_url = path_url1.replace("/", "");
 
 console.log('/* javascript login | VARIABLES CONSTANTES*/');
-var api_login = 'http://localhost/MisSitios/mandragoraJSF/api/login/';
+var api_login = (host!='localhost')?'https://portafolio1.webcindario.com/api/login/':'http://localhost/MisSitios/mandragoraJSF/api/login/';
 console.log('api_login='+api_login);
 
 function btnLogin(){
