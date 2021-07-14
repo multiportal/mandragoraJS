810d03de6d29d108f45f4ac4d5baf535eb5e8ed0
Memo Jimenez <memojl08@gmail.com>
2021-07-13 21:35:37 -0500

    update 13.07.2021

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 /* 
 CMS-Javascript (spa) 
 Autor: Guillermo Jiménez López
-Fecha de Actualización: 11.07.2021
+Fecha de Actualización: 14.07.2021
 Versión: 1.0.0
 */
 //import './assets/bootstrap-5.0.2/css/bootstrap.min.css';
 import './assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
+
+
+
