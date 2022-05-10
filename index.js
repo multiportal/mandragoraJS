e5b021cb6fe88a62b52a73ed515a299b8c22bf23
memojl <memojl08@gmail.com>
2022-05-10 15:18:51 -0500

    update 10.05.22.15.18

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 /* 
 CMS-Javascript (spa) 
 Autor: Guillermo Jiménez López
-Fecha de Actualización: 14.02.2022
-Versión: 1.0.2
+Fecha de Actualización: 14.03.2022
+Versión: 1.0.3
 */
+//Files import for webpack
 //import './src/assets/bootstrap-5.0.2/css/bootstrap.min.css';
 import './src/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
+//Files import for App
 import {consola} from './src/app/console.js';
 import {inicio,variables} from './src/app/lib.js';
 import {reMod,menuWeb,consoleLocal} from './src/app/functions.js';
 import {no_menu_web,menu_web,routes_session,router,controlRoutes} from './src/routes/index.routes.js';
 
-
+//HASHCHANGE EVENT LISTENER FOR APP
 window.addEventListener('hashchange',()=>{consoleLocal('warn','Event Listener');
     const v = variables();
     const {hash,URL,pag_name,vars_Url,mod,ext,id,ext2,route,hash2,url_mod,url_m,url404} = v;
-    menuWeb(hash2,no_menu_web,menu_web);
-    router(hash,hash2,url_mod,url404);
+    menuWeb(hash2,no_menu_web,menu_web); //Menu web
+    router(hash,hash2,url_mod,url404); //Routes
     controlRoutes(route,id);
-
+    //Redirect to main page
     reMod(mod);
     consoleLocal('log',consola(v));
 });
