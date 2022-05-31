8245124bb9f4f8e825f95b7d30da8d0bc15bb866
FrontHitss <programador.hitss@gmail.com>
2022-05-30 21:50:32 -0500

    update 30.05.22.21.50

---
+++ src/controllers/index.js
@@ -0,0 +1,11 @@
+import Home from "./home";
+import nosotros from "./nosotros";
+import NotFound from "./404";
+
+const pages = {
+  home: Home,
+  nosotros: nosotros,
+  notFound: NotFound,
+};
+
+export { pages };
