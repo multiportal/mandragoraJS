5c07024144848db796707c9ea2ce4ba888c0cf71
FrontHitss <programador.hitss@gmail.com>
2022-06-05 21:19:00 -0500

    update 05.06.22.21.18

--- src/index.js
+++ src/index.js
@@ -10,20 +10,13 @@ import './assets/bootstrap-5.0.2/css/bootstrap.min.css';
 import './assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 //Files import for App
 import {consola} from './app/console.js';
-import {inicio,variables} from './app/lib.js';
+import {inicio,load,variables} from './app/lib.js';
 import {reMod,menuWeb,consoleLocal} from './app/functions.js';
 import {no_menu_web,menu_web,routes_session,router,controlRoutes} from './routes/index.routes.js';
 
 //HASHCHANGE EVENT LISTENER FOR APP
 window.addEventListener('hashchange',()=>{consoleLocal('warn','Event Listener');
-    const v = variables();
-    const {hash,URL,pag_name,vars_Url,mod,ext,id,ext2,route,hash2,url_mod,url_m,url404} = v;
-    menuWeb(hash2,no_menu_web,menu_web); //Menu web
-    router(hash,hash2,url_mod,url404); //Routes
-    controlRoutes(route,id);
-    //Redirect to main page
-    reMod(mod);
-    consoleLocal('log',consola(v));
+    load();
 });
 
 inicio();
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: "eval-source-map",
+};
