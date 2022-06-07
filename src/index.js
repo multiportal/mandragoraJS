e58c31c4f1c60774852c1a25b3a2ed7b13d441ee
FrontHitss <programador.hitss@gmail.com>
2022-06-07 12:07:04 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* 
 CMS-Javascript (spa) 
 Autor: Guillermo Jiménez López
-Fecha de Actualización: 14.03.2022
-Versión: 1.0.3
+Fecha de Actualización: 07.06.2022
+Versión: 1.0.4
 */
 //Files import for webpack
 import './assets/bootstrap-5.0.2/css/bootstrap.min.css';
 import './assets/css/dashboard.css'; //** 3 WARNINGS IN WEBPACK FOR BUILD */
-import './assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 import './assets/main.scss';
+import './assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 //Files import for App
 import {inicio,load} from './app/lib';
 import {consoleLocal} from './app/functions';
