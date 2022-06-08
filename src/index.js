707f82ffc9f48276825efc855afe3c24fc15c2ba
FrontHitss <programador.hitss@gmail.com>
2022-06-08 17:03:03 -0500

    update 08.06.22.17.02

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 /* 
 CMS-Javascript (spa) 
 Autor: Guillermo Jiménez López
-Fecha de Actualización: 14.03.2022
-Versión: 1.0.3
+Fecha de Actualización: 07.06.2022
+Versión: 1.0.4
 */
 //Files import for webpack
 import './assets/bootstrap-5.0.2/css/bootstrap.min.css';
-//import './assets/css/dashboard.css'; //** 3 WARNINGS IN WEBPACK FOR BUILD */
+import './assets/css/dashboard.css'; //** 3 WARNINGS IN WEBPACK FOR BUILD */
+import './assets/main.scss';
 import './assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 //Files import for App
-import {inicio,load} from './app/lib.js';
-import {consoleLocal} from './app/functions.js';
+import {inicio,load} from './app/lib';
+import {consoleLocal} from './app/functions';
 
 //HASHCHANGE EVENT LISTENER FOR APP
 window.addEventListener('hashchange',()=>{consoleLocal('warn','Event Listener');
