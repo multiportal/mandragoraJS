d5eee3d3825fb8dda6ad7127e867cf0ac2a0d053
FrontHitss <programador.hitss@gmail.com>
2022-06-08 11:06:07 -0500

    update 08.06.22

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ Versión: 1.0.4
 //import './src/assets/main.scss';
 import './src/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 //Files import for App
-import {inicio,load} from './src/app/lib';
-import {consoleLocal} from './src/app/functions';
+import {inicio,load} from './src/app/lib.js';
+import {consoleLocal} from './src/app/functions.js';
 
 //HASHCHANGE EVENT LISTENER FOR APP
 window.addEventListener('hashchange',()=>{consoleLocal('warn','Event Listener');
