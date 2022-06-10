f8931c575f158e4a27712c00c1cc3fd43e129146
FrontHitss <programador.hitss@gmail.com>
2022-06-10 09:51:39 -0500

    Update SERVICE WORKER

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,6 +15,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
   module: {
