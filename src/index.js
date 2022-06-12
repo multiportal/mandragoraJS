e54cfed9012bb50f4f89be3654608a7935ec5a9c
FrontHitss <programador.hitss@gmail.com>
2022-06-12 13:06:48 -0500

    update 12.06.22.13.06

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /* 
 CMS-Javascript (spa) 
 Autor: Guillermo Jiménez López
-Fecha de Actualización: 11.06.2022
-Versión: 1.0.5
+Fecha de Actualización: 12.06.2022
+Versión: 1.0.6
 */
 //Files import for webpack
 import './assets/bootstrap-5.0.2/css/bootstrap.min.css';
