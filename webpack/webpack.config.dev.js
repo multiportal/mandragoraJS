b5d33dca799c57d632a8c9dc232be165d56f3ecc
FrontHitss <programador.hitss@gmail.com>
2022-06-18 17:56:51 -0500

    update webpack

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -21,6 +21,8 @@ module.exports = {
       patterns: [
         /*{ from: 'src/assets/pwa/sw.js', to: 'sw.js' },*/
         /*{ from: 'src/assets/pwa/icon/', to: 'icon/' },*/
+        /*{ from: 'src/assets/pwa/icon/', to: 'assets/pwa/icon/icon/' },*/
+        /*{ from: 'src/assets/img', to: 'assets/img' },*/
         { from: 'src/assets/pwa/icon/apple-icon-152x152.png', to: 'icon/apple-icon-152x152.png' },
         { from: 'src/assets/pwa/icon/apple-icon-180x180.png', to: 'icon/apple-icon-180x180.png' },
       ],
