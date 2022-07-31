ce1166cc129c51848375bc79e26a8f642e21048b
FrontHitss <programador.hitss@gmail.com>
2022-07-30 22:44:08 -0500

    update webpack

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,6 +15,16 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        //{ from: 'src/assets/pwa/sw.js', to: 'sw.js' },
+        //{ from: 'src/assets/pwa/icon/apple-icon-152x152.png', to: 'icon/apple-icon-152x152.png' },
+        //{ from: 'src/assets/pwa/icon/apple-icon-180x180.png', to: 'icon/apple-icon-180x180.png' },
+        //{ from: 'src/assets/pwa/icon/', to: 'assets/pwa/icon/icon/' },
+        { from: 'src/assets/img/', to: 'src/assets/img/' },
+        { from: 'src/pages', to: 'src/pages' },
+      ],
     }),
   ],
   module: {
