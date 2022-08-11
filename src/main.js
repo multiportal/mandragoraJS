0c809279edcc856b8d4123a330bb0c96b238251e
FrontHitss <programador.hitss@gmail.com>
2022-08-11 10:00:20 -0500

    update server

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 var colors = require('colors');
-const port = 3000;
+const port = 4000;
 
 colors.enable()
-app.use(express.static(__dirname +'build'));
+app.use(express.static('build'));
+//app.use(express.static(__dirname +'/build'));
 //app.get("/", (req, res) => {res.send("Root");});
 
 app.listen(port, () => {
