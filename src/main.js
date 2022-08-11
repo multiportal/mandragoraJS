60c6945de63474f52aedb037f1fe07ce5fe73dcd
memojl <memojl08@gmail.com>
2022-08-10 22:55:50 -0500

    update for heroku

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+var colors = require('colors');
+const port = 4000;
+
+colors.enable()
+
+app.use(express.static('build'));
+
+app.get("/", (req, res) => {res.send("Root");});
+
+app.listen(port, () => {
+    console.log(`Server on port:${port}`.blue);
+});
