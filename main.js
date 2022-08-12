a376fab6480a05365ac6cb5fa0bb7343aeb7d719
FrontHitss <programador.hitss@gmail.com>
2022-08-12 11:58:21 -0500

    Sin 404 server

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ var colors = require('colors');
 colors.enable();
 
 app.use(express.static(__dirname + '/build'));
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/build/src/pages/404.html');
-});
+//app.use((req, res, next) => {res.status(404).sendFile(__dirname + '/build/src/pages/404.html');});
 
 app.listen(PORT, () => {
     console.log(`Server on PORT:${PORT}`.blue);
