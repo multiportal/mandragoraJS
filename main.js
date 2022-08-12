49d6381e517f8f84da24469f8173c51ffc3329d8
FrontHitss <programador.hitss@gmail.com>
2022-08-11 19:42:13 -0500

    update server - start

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9000;
+var colors = require('colors');
+colors.enable();
+
+app.use(express.static(__dirname + '/build'));
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/build/404.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server on PORT:${PORT}`.blue);
+});
