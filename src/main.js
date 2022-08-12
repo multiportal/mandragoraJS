a8d099982142d038da18c528fd3664cd1ab19952
FrontHitss <programador.hitss@gmail.com>
2022-08-12 12:43:02 -0500

    Update Del 404 & server

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ const PORT = process.env.PORT || 9000;
 var colors = require('colors');
 colors.enable();
 
-app.use(express.static(__dirname + '/build'));
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/build/404.html');
-});
+app.use(express.static(__dirname + './../build'));
+//app.use((req, res, next) => {res.status(404).sendFile(__dirname + '/build/pages/404.html');});
 
 app.listen(PORT, () => {
     console.log(`Server on PORT:${PORT}`.blue);
