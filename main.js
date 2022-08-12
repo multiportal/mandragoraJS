a35f16d72e3bb9c2a8b028f0adb9901ca704f5d6
FrontHitss <programador.hitss@gmail.com>
2022-08-11 19:31:31 -0500

    update port

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 9000;
 var colors = require('colors');
 colors.enable();
 
