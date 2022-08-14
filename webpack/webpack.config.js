46e99bd84882b9cbbd6318da4a5fd5be09f0fd76
FrontHitss <programador.hitss@gmail.com>
2022-08-14 11:36:53 -0500

    update webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -20,8 +20,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: 'src/assets/pwa/sw.js', to: 'sw.js' },
-        { from: 'src/assets/pwa/icon/apple-icon-152x152.png', to: 'icon/apple-icon-152x152.png' },
-        { from: 'src/assets/pwa/icon/apple-icon-180x180.png', to: 'icon/apple-icon-180x180.png' },
+        { from: 'src/assets/pwa/manifest.json', to: 'assets/pwa/manifest.json'},
         { from: 'src/assets/pwa/icon/', to: 'assets/pwa/icon/' },
         { from: 'src/assets/img/', to: 'assets/img/' },
       ],
