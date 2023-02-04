d037fbb130e9e9f7a7a60dd7e6a883c259d34649
memojl <memojl08@gmail.com>
2023-02-04 16:34:40 -0600

    Update Api web

--- src/app/urls.js
+++ src/app/urls.js
@@ -1,6 +1,6 @@
 const {host} = window.location;
 /* URL-API */
-export const Api = (host!='localhost:9001')?'https://apirestm.000webhostapp.com/api':'http://localhost/MisSitios/apirestm/api';
-//export const Api = 'https://apirestm.000webhostapp.com/api';
+export const Api = (host!='localhost:9001')?'https://apirestm.webcindario.com/api':'http://localhost/MisSitios/apirestm/api';
+//export const Api = 'https://apirestm.webcindario.com/api';
 /* URL-LINKS */
 export const api_links = Api + '/v2/links/';
