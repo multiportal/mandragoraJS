a113db8c52df5971c5f18ac919682053daffc91d
memojl <memojl08@gmail.com>
2023-02-19 16:39:05 -0600

    update src/assets

--- vite-app/src/main.js
+++ vite-app/src/main.js
@@ -1,23 +1,8 @@
-import './assets/css/style.css'
-import javascriptLogo from './assets/img/javascript.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+/* 
+CMS-Javascript (spa) 
+Autor: Guillermo Jiménez López
+Fecha de Actualización: 19.02.2023
+Versión: 1.2.1 - VITE
+*/
+//Files import for webpack
+import './assets/bootstrap-5.0.2/css/bootstrap.min.css';
