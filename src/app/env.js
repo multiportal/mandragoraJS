ea71fb007f7b80fd885de08dccac6f3e0c4a491c
memojl <memojl08@gmail.com>
2023-03-07 08:30:06 -0600

    Update New

--- src/app/env.js
+++
@@ -1,7 +0,0 @@
-/*VARIABLES DE ENTORNO*/
-export const title = 'MandragoraJS SPA';
-export const proyecto = 'mandragoraJS'; //PROYECTO
-export const path_hash = '#/'; //path_hash
-export const path_src = 'src/'; //RESOURCE PATH
-export const path_page = path_src + 'pages/'; //PAGE PATH
-export const hostTest = '127.0.0.1:5173';
