0164510f3ca4afb8370f5bf277bd09da57932bf5
memojl <memojl08@gmail.com>
2023-03-07 09:54:48 -0600

    Update MandragoraJs

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ import '/public/assets/css/style.css';
 import '/public/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 //import '/public/assets/js/sweetalert2.all.min.js';
 //import '/public/assets/pwa/sw.js'; //SERVICE WORKER
-//import '/public/assets/pwa/appCon.js';
+import '/public/assets/pwa/appCon.js';
 //Files import for App
-//import { inicio, load } from './app/lib.js';
-//import { consoleLocal } from './app/functions.js';
-
+import { inicio, load } from './src/app/lib.js';
+import { consoleLocal } from './src/app/functions.js';
 
 //HASHCHANGE EVENT LISTENER FOR APP
 window.addEventListener('hashchange', () => {
     consoleLocal('warn', 'Event Listener');
-    //load();
+    load();
 });
 
-//inicio();
-
+inicio();
--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,6 +1,6 @@
 import { getModules } from "../functions";
 import { pages, pagesSys } from "./pages";
-import NotFound from "../../pages/404/index.html";
+import NotFound from "../../pages/404/index.html?raw";
 
 export default (page) => {
   const pagesAll = {...pages, ...pagesSys}//console.log('pagesAll:',pagesAll);
