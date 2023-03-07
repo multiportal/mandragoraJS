25129c7b4c8f38de68def07047e2c8494bdb21ac
memojl <memojl08@gmail.com>
2023-03-07 10:30:37 -0600

    Update appCon

--- public/assets/pwa/appCon.js
+++ public/assets/pwa/appCon.js
@@ -1,6 +1,6 @@
 //appCon.js index.php
 let loc = window.location;
-const {	pathname, host } = loc; console.log('host:',host)
+const {	pathname, host } = loc; //console.log('host:',host);
 if (host !== 'localhost' && host !== '127.0.0.1:5173') {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.register(pathname + 'sw.js').then(function (registration) {
