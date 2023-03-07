74c9e155b85a74f9ab41eff4c72d3b6e73169c72
memojl <memojl08@gmail.com>
2023-03-06 18:47:37 -0600

    Update Test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import {
-  defineConfig
-} from 'vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  assetsInclude: ['**/*.html'],
+  assetsInclude: ['**.html'],
+  /*
   root: './',
   build: {
     outDir: 'dist',
   },
   publicDir: 'public',
+  */
 })
