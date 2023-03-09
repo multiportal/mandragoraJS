343a51eae59be1ad7b18472f6f8242b69bf9fcb2
memojl <memojl08@gmail.com>
2023-03-09 14:27:55 -0600

    Update Links

--- src/app/controllers/links.js
+++ src/app/controllers/links.js
@@ -1,54 +1,54 @@
 //LINKS.JS
 import { consoleLocal } from '../functions';
-import { fetchProfile } from '../services/fetch';
 import { variables } from '../lib';
-import { api_links, Api } from '../const.env';
+import { api_links } from '../const.env';
 
 /* VARIABLES CONSTANTES*/
 const { host, dominio, path_url, path_build, hostDev } = variables();
 console.log('/* javascript Links | VARIABLES CONSTANTES*/');
 //const api_links = Api + '/v2/links/';
-consoleLocal('log','api_links=' + api_links);
+consoleLocal('log', 'api_links=' + api_links);
 ////////////////////////
 
 const linksList = async () => {
   let list = document.querySelector('#list');
   list.innerHTML = ``;
+  let noResult = `
+    <div class="col-md-4 mx-auto">
+      <div class="card card-body text-center">
+        <p>No hay links agregados todavia.</p>
+        <a href="#/links/add">¡Crea uno!</a>
+        </div>
+    </div>`;
   let url_get = api_links;
+  console.log(url_get);
   const response = await fetch(url_get); //console.log(response.status);
-  if(response.status == 200){
-  const { data } = await response.json(); //console.log(data);
-  data.forEach(element => {
-    const { ID, title, url, description, cate, user_id, created_at } = element;
-    if (element) {
-      list.innerHTML += `
-        <!--div class="col-md-4"-->
-          <div id="${ID}" class="card" style="width: 18rem;">
-            <img src="${path_build}assets/img/webpage.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <a href="${url}" target="_blank">
-                <h5 class="card-title">${title}</h5>
-              </a>
-              <p class="m-2">${cate} - ${created_at}</p>
-              <p class="card-text">${description}</p>
-              <a class="btn btn-secondary" href="#/links/linksEdit/${ID}"><i class="fa fa-edit"></i> Editar</a>
-              <button data-id="${ID}" class="btn btn-danger"><i class="fa fa-trash"></i> Borrar</button>
+  if (response.status == 200) {
+    const { data } = await response.json(); console.log(data);
+    if(data){
+      data.forEach(element => {
+        const { ID, title, url, description, cate, user_id, created_at } = element;
+        //if (element) {
+        list.innerHTML += `
+          <!--div class="col-md-4"-->
+            <div id="${ID}" class="card" style="width: 18rem;">
+              <img src="${path_build}assets/img/webpage.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <a href="${url}" target="_blank">
+                  <h5 class="card-title">${title}</h5>
+                </a>
+                <p class="m-2">${cate} - ${created_at}</p>
+                <p class="card-text">${description}</p>
+                <a class="btn btn-secondary" href="#/links/linksEdit/${ID}"><i class="fa fa-edit"></i> Editar</a>
+                <button data-id="${ID}" class="btn btn-danger"><i class="fa fa-trash"></i> Borrar</button>
+              </div>
             </div>
-          </div>
-        <!--/div-->
-      `;
-    } else {
-      list.innerHTML += `
-      <div class="col-md-4 mx-auto">
-          <div class="card card-body text-center">
-              <p>There are not Links saved yet.</p>
-              <a href="#/links/add">Create One!</a>
-          </div>
-      </div>
-      `;
-    }
-  });
-  } else if(response.status == 404){ 
+          <!--/div-->
+        `;
+        //} else { list.innerHTML = noResult; }
+      });
+    } else { list.innerHTML = noResult; }
+  } else if (response.status == 404) {
     console.error('STATUS 404: Lista no encontrada!');
     list.innerHTML = `<div>Lista no encontrada!</div>`;
   }
@@ -68,12 +68,12 @@ const linksList = async () => {
         cancelButtonColor: '#3085d6',
         confirmButtonText: 'Borrar'
       }).then((result) => {
-        if (result.value) {//console.log('Link Borrado');        
+        if (result.value) { //console.log('Link Borrado');        
           var datos = {
             //ID: id,
             token: Token
           }
-          const url_delete = api_links + id;//console.warn(url_post);  
+          const url_delete = api_links + id; //console.warn(url_post);  
           fetch(url_delete, {
             method: 'DELETE',
             headers: {
@@ -97,7 +97,7 @@ function borrar() {
   console.log('Click Eliminar');
   setTimeout(function () {
     console.log('links Recargado');
-    linksList();//window.location.href = dominio + path_url + '#/links';
+    linksList(); //window.location.href = dominio + path_url + '#/links';
   }, 2000);
 }
 
