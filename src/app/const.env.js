aa628a8d7ee86b3938ea4187e50cde67236d9ba3
memojl <memojl08@gmail.com>
2023-05-19 09:59:39 -0600

    Update const

--- src/app/const.env.js
+++ src/app/const.env.js
@@ -8,7 +8,7 @@ export const path_page = path_src + 'pages/'; //PATH PAGE
 export const typeDev = 'vite'; //vite or webpack
 export const hostDev = (typeDev == 'vite') ? '127.0.0.1:5173' : 'localhost:9001';
 export const hostPre = (typeDev == 'vite') ? '127.0.0.1:4173' : 'localhost';
-/* URL-API */
-export const Api = (host!=hostDev)?'https://apirestm.000webhostapp.com/api':'http://localhost/MisSitios/apirestm/api';
+/* URL-API */ //https://apirestm.000webhostapp.com/api
+export const Api = (host!=hostDev)?'https://apirestm.webcindario.com/api':'http://localhost/MisSitios/apirestm/api';
 export const api_links = Api + '/v2/links/';/* URL-LINKS */
 export const apiVer = Api + '/v2/api_version';
