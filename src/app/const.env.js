58b9e8c0da5322fdc0964e7724411db38022a7a5
memojl <memojl08@gmail.com>
2023-12-25 15:16:12 -0600

    Update env

--- src/app/const.env.js
+++ src/app/const.env.js
@@ -2,16 +2,17 @@ const {host} = window.location;
 /*VARIABLES DE ENTORNO*/
 export const title = 'MandragoraJS SPA';
 //export const proyecto = 'mandragoraJS'; //PROYECTO
+export const typeBack = 'firebase'; //firebase / api
 export const path_hash = '#/'; //PATH HASH
 export const path_src = 'src/'; //PATH RESOURCE 
 export const path_page = path_src + 'pages/'; //PATH PAGE
 export const typeDev = 'vite'; //vite or webpack
 export const hostDev = (typeDev == 'vite') ? 'localhost:5173' : 'localhost:9001';
-//export const hostPre = (typeDev == 'vite') ? '127.0.0.1:4173' : 'localhost';
-export const hostPre = 'localhost';
+export const hostPre = 'localhost';//(typeDev == 'vite') ? '127.0.0.1:4173' : 'localhost';
 /* URL-API */ //https://apirestm.000webhostapp.com/api //
 console.log(host+'=='+hostDev,host+'=='+hostPre);
 export const Api = (host==hostDev || host==hostPre)?'http://localhost/MisSitios/apirestm/api':'https://apirestm.000webhostapp.com/api';
 console.warn('Api='+Api);
+export const apiVer = Api + '/v2/api_version';/* URL-VERSION */
+/* */
 export const api_links = Api + '/v2/links/';/* URL-LINKS */
-export const apiVer = Api + '/v2/api_version';
