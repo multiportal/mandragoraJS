617383496b7c334f9fbab1bfc741b90dd8606dd6
memojl <memojl08@gmail.com>
2023-12-24 23:51:46 -0600

    Update Const + Fuctions + Web

--- src/app/const.env.js
+++ src/app/const.env.js
@@ -6,10 +6,10 @@ export const path_hash = '#/'; //PATH HASH
 export const path_src = 'src/'; //PATH RESOURCE 
 export const path_page = path_src + 'pages/'; //PATH PAGE
 export const typeDev = 'vite'; //vite or webpack
-export const hostDev = (typeDev == 'vite') ? '127.0.0.1:5173' : 'localhost:9001';
+export const hostDev = (typeDev == 'vite') ? 'localhost:5173' : 'localhost:9001';
 export const hostPre = (typeDev == 'vite') ? '127.0.0.1:4173' : 'localhost';
-/* URL-API */ //https://apirestm.000webhostapp.com/api
-//export const Api = (host!=hostDev)?'https://apirestm.000webhostapp.com/api':'http://localhost/MisSitios/apirestm/api';
-export const Api = (host=='localhost')?'http://localhost/MisSitios/apirestm/api':'https://apirestm.000webhostapp.com/api';
+/* URL-API */ //https://apirestm.000webhostapp.com/api //console.log(host+'=='+hostDev);
+export const Api = (host!=hostDev)?'https://apirestm.000webhostapp.com/api':'http://localhost/MisSitios/apirestm/api';
+console.warn('Api='+Api);
 export const api_links = Api + '/v2/links/';/* URL-LINKS */
 export const apiVer = Api + '/v2/api_version';
