ce03d778cf3b48a31c49ea0171bd8d53f93984a8
multiportal <multiportal@outlook.com>
2024-04-13 09:03:05 -0600

    Update Web

--- src/app/core/const.env.js
+++ src/app/core/const.env.js
@@ -13,7 +13,7 @@ export const hostPre = 'localhost';//(typeDev == 'vite') ? '127.0.0.1:4173' : 'l
 export const Api = (host==hostDev || host==hostPre)?'http://localhost/MisSitios/apirestm/api':'https://apirestm.000webhostapp.com/api';
 export const apiVer = Api + '/v2/api_version';/* URL-VERSION */
 /* */
-//export const api_links = Api + '/v2/links/';/* URL-LINKS */
+export const api_links = Api + '/v2/links/';/* URL-LINKS */
 console.log(host+'=='+hostDev,host+'=='+hostPre);
 console.warn('Api='+Api);
 if(typeBack!='firebase'){
