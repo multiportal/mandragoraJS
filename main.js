4247e7496f8c9067d71db14ba28aa2035e55d0aa
multiportal <multiportal@outlook.com>
2026-07-01 22:26:58 -0600

    UPDATE PUBLIC

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ Versión: 0.0.1 - MandragoraJS - Vite
 Notas: 
 */
 //Files import for App
-import './publicDir/assets/bootstrap-5.0.2/css/bootstrap.min.css';
-import './publicDir/assets/css/dashboard.css'; //** 3 WARNINGS IN WEBPACK FOR BUILD -->ADD DOM */
-import './publicDir/assets/font-awesome-5.14.0/css/all.min.css';
-import './publicDir/assets/css/style.css';
-import './publicDir/assets/css/load.css';
-import './publicDir/assets/js/jquery-3.3.1.slim.min.js';
-import './publicDir/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
-//import './publicDir/assets/js/sweetalert2.all.min.js';
-//import './publicDir/assets/pwa/sw.js'; //SERVICE WORKER
-import './publicDir/assets/pwa/appCon.js';
+import './public/assets/bootstrap-5.0.2/css/bootstrap.min.css';
+import './public/assets/css/dashboard.css'; //** 3 WARNINGS IN WEBPACK FOR BUILD -->ADD DOM */
+import './public/assets/font-awesome-5.14.0/css/all.min.css';
+import './public/assets/css/style.css';
+import './public/assets/css/load.css';
+import './public/assets/js/jquery-3.3.1.slim.min.js';
+import './public/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
+//import './public/assets/js/sweetalert2.all.min.js';
+//import './public/assets/pwa/sw.js'; //SERVICE WORKER
+import './public/assets/pwa/appCon.js';
 //Import Functions JS for App
 import { load } from "./src/app/functions.js";
 //Functions Application
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   base: './',
   root: './',
   build: {
-    outDir: 'public',
+    outDir: 'web',
   },
-  publicDir: 'publicDir',
+  publicDir: 'public',
 })
